c9096514713f27c6cc2334b3a334eedd8de7e8d2
verybon <verybon@users.noreply.github.com>
2021-04-20 10:55:05 +0900

    Create nkor_0201_08_0506_510.js

---
+++ nkor_0201_08_0506/nkor_0201_08_0506_510.js
@@ -0,0 +1,27 @@
+function addModules() {
+    var modules = {
+        narration: function(modules, page) {
+            var module = createNarration({
+                moduleName: "NARRATION",
+                box: amt.get(".narration", page),
+                audioList: ["./media/audios/nkor_0201_08_0506_510.mp3"],
+                sceneTimes: [
+                    { start: 0, end: 20 },
+                    { start: 20, end: 300 }
+                ]
+                // ,
+                // textTimes: [
+                //     { start: 5, end: 7},
+                //     { start: 7, end: 8},
+                //     { start: 8, end: 10},
+                //     { start: 10, end: 12},
+                //     { start: 12, end: 15},
+                //     { start: 15, end: 100}
+                // ]
+            })
+            module.start();
+            modules.push(module);
+        }
+    }
+    return modules;
+}
