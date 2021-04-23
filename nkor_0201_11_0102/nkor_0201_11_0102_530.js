e94e169346557c4416062e077fd85c7d1f47bdd0
verybon <verybon@users.noreply.github.com>
2021-04-23 17:44:36 +0900

    Add files via upload

---
+++ nkor_0201_11_0102/nkor_0201_11_0102_530.js
@@ -0,0 +1,21 @@
+function addModules() {
+    var modules = {
+        video: function(modules, page) {
+            var module = createVideoContent({
+                moduleName: "VIDEO_CONTENT",
+                box: page,
+                src: "./media/videos/nkpt_531.mp4",
+                sceneTimes: [
+                    { start: 1, end: 24 },
+                    { start: 24, end: 41 },
+                    { start: 41, end: 66 },
+                    { start: 66, end: 85 },
+                    { start: 85, end: 1000 }
+                ]
+            })
+            module.start();
+            modules.push(module);
+        }
+    }
+    return modules;
+}
